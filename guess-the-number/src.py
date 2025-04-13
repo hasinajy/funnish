e51ebdc25515524e05eda2c818bef26a819cdf6c
hasinajy <jyhasina@gmail.com>
2025-04-13 11:38:02 +0300

    feat/gtn: Allow the user to play again when the hidden number is guessed

--- guess-the-number/src.py
+++ guess-the-number/src.py
@@ -7,7 +7,7 @@ def display_feedback(feedback: str) -> None:
 
 def prompt_number(prompt: str) -> int:
     try:
-        return int(input(f"{prompt}: "))
+        return int(input(f"{prompt} "))
     except ValueError:
         display_feedback(feedback="Please enter a valid number.")
         return prompt_number(prompt=prompt)
@@ -23,7 +23,7 @@ def prompt_difficulty() -> int:
     print("\t2. Medium (0 - 500)")
     print("\t3. Hard (0 - 1000)")
     print("\t4. Custom range")
-    difficulty_id = prompt_number(prompt="Enter the difficulty you want to play") - 1
+    difficulty_id = prompt_number(prompt="Enter the difficulty you want to play:") - 1
     
     difficulties = ["Easy", "Medium", "Hard", "Custom"]
     if difficulty_id < 0 or difficulty_id >= len(difficulties): 
@@ -39,9 +39,24 @@ def get_number_to_guess(difficulty_id: int) -> int:
     if difficulty_id < len(ranges): 
         lower_bound, higher_bound = ranges[difficulty_id]
     else:
-        lower_bound = prompt_number(prompt="Enter the lower bound")
-        higher_bound = prompt_number(prompt="Enter the higher bound")
+        lower_bound = prompt_number(prompt="Enter the lower bound:")
+        higher_bound = prompt_number(prompt="Enter the higher bound:")
     return randint(lower_bound, higher_bound)
+
+
+def prompt_yn(prompt: str) -> str:
+    user_choice = input(f"{prompt} (y/n) : ")
+    if user_choice.lower() not in ["y", "n"]:
+        display_feedback(feedback="Please enter a valid choice.")
+        user_choice = prompt_yn(prompt=prompt)
+    return user_choice
+
+
+def process_play_again(again_yn: str) -> None:
+    if again_yn == "y":
+        play()
+    else:
+        display_feedback(feedback="Bye!")
 
 
 def play() -> None:
@@ -51,7 +66,7 @@ def play() -> None:
     to_guess = get_number_to_guess(difficulty_id=difficulty_id)
 
     while not game_over:
-        user_guess = prompt_number(prompt="Enter your guess")
+        user_guess = prompt_number(prompt="Enter your guess:")
         if user_guess < to_guess:
             display_feedback(feedback="The hidden number is higher.")
         elif user_guess > to_guess:
@@ -59,7 +74,10 @@ def play() -> None:
         else:
             display_feedback(feedback="Congratulations! You guessed the hidden number.")
             game_over = True
-            
-            
+    
+    again_yn = prompt_yn(prompt="Would you like to play again?")
+    process_play_again(again_yn=again_yn) 
+
+
 if __name__ == "__main__":
     play()
