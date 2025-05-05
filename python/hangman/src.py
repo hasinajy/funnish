17755465aa91b360c9dfff81e03fb2e3a453c0a3
hasinajy <jyhasina@gmail.com>
2025-05-06 00:24:53 +0300

    refactor/hg: Extract the game main logic to the play function

--- python/hangman/src.py
+++ python/hangman/src.py
@@ -23,31 +23,36 @@ def display_title() -> None:
     print("Welcome to HANGMAN")
 
 
-WORD_DICTIONARY = ["python", "java", "javascript", "typescript"]
-MAX_MISTAKES = 3
+def play() -> None:
+    WORD_DICTIONARY = ["python", "java", "javascript", "typescript"]
+    MAX_MISTAKES = 3
 
-current_mistakes = 0
-word_to_guess = sample(population=WORD_DICTIONARY, k=1)[0]
-guessed_characters = []
-current_word_state: str = "_" * len(word_to_guess)
+    current_mistakes = 0
+    word_to_guess = sample(population=WORD_DICTIONARY, k=1)[0]
+    guessed_characters = []
+    current_word_state: str = "_" * len(word_to_guess)
 
-display_title()
+    display_title()
 
-while current_mistakes < MAX_MISTAKES:
-    print("The current word state:", current_word_state)
-    user_guess = prompt_guess()
-    if user_guess in guessed_characters:
-        print("You have already guessed the given character.")
-    elif user_guess in word_to_guess:
-        guessed_characters.append(user_guess)
-        current_word_state = get_word_state(
-            to_guess=word_to_guess, guessed_characters=guessed_characters
-        )
-        if current_word_state == word_to_guess:
-            print("Congratulations! You found the hidden word.")
-            break
-    else:
-        current_mistakes = current_mistakes + 1
-        print("The given character is not found in the hidden word.")
-        if current_mistakes == MAX_MISTAKES:
-            print("Max mistakes reached. Try again next time!")
+    while current_mistakes < MAX_MISTAKES:
+        print("The current word state:", current_word_state)
+        user_guess = prompt_guess()
+        if user_guess in guessed_characters:
+            print("You have already guessed the given character.")
+        elif user_guess in word_to_guess:
+            guessed_characters.append(user_guess)
+            current_word_state = get_word_state(
+                to_guess=word_to_guess, guessed_characters=guessed_characters
+            )
+            if current_word_state == word_to_guess:
+                print("Congratulations! You found the hidden word:", current_word_state)
+                break
+        else:
+            current_mistakes = current_mistakes + 1
+            print("The given character is not found in the hidden word.")
+            if current_mistakes == MAX_MISTAKES:
+                print("Max mistakes reached. Try again next time!")
+
+
+if __name__ == "__main__":
+    play()
