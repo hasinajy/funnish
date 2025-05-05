1f7b71b527496ec82608b27ee7af071af50490a4
hasinajy <jyhasina@gmail.com>
2025-05-06 00:18:08 +0300

    feat/hg: Get the hidden word from a dictionary and improve game feedback

--- python/hangman/src.py
+++ python/hangman/src.py
@@ -1,29 +1,53 @@
+from random import sample
+
+
 def prompt_guess() -> None:
-    user_guess = input("Enter your guess: ")
+    user_guess = input("Enter your guess (one character): ")
     if len(user_guess) > 1:
         print("Please enter one character.")
         user_guess = prompt_guess()
     return user_guess
 
 
-def display_word_state(to_guess: str, guessed_characters: list) -> None:
+def get_word_state(to_guess: str, guessed_characters: list) -> str:
+    word_state = ""
     for char in to_guess:
         if char in guessed_characters:
-            print(char, end="")
+            word_state = word_state + char
         else:
-            print("_", end="")
-    print()
+            word_state = word_state + "_"
+    return word_state
 
 
-WORD_TO_GUESS = "hangman"
-hp = 7
-correct_guesses = []
+def display_title() -> None:
+    print("Welcome to HANGMAN")
 
-while hp:
+
+WORD_DICTIONARY = ["python", "java", "javascript", "typescript"]
+MAX_MISTAKES = 3
+
+current_mistakes = 0
+word_to_guess = sample(population=WORD_DICTIONARY, k=1)[0]
+guessed_characters = []
+current_word_state: str = "_" * len(word_to_guess)
+
+display_title()
+
+while current_mistakes < MAX_MISTAKES:
+    print("The current word state:", current_word_state)
     user_guess = prompt_guess()
-    if user_guess not in WORD_TO_GUESS:
-        print("The hidden does not contain the specified character.")
-        hp = hp - 1
+    if user_guess in guessed_characters:
+        print("You have already guessed the given character.")
+    elif user_guess in word_to_guess:
+        guessed_characters.append(user_guess)
+        current_word_state = get_word_state(
+            to_guess=word_to_guess, guessed_characters=guessed_characters
+        )
+        if current_word_state == word_to_guess:
+            print("Congratulations! You found the hidden word.")
+            break
     else:
-        correct_guesses.append(user_guess)
-        display_word_state(to_guess=WORD_TO_GUESS, guessed_characters=correct_guesses)
+        current_mistakes = current_mistakes + 1
+        print("The given character is not found in the hidden word.")
+        if current_mistakes == MAX_MISTAKES:
+            print("Max mistakes reached. Try again next time!")
