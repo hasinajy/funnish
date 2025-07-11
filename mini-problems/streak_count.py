8687da7c3eca01762d1569b08e5c6c2dc33ac956
hasinajy <jyhasina@gmail.com>
2025-07-11 14:09:14 +0300

    feat(streak_count): Implement threshold value for a streak to be valid

--- mini-problems/streak_count.py
+++ mini-problems/streak_count.py
@@ -1,8 +1,9 @@
 # Parameters
 MAX_MISS_COUNT = 0
+THRESHOLD = 2
 
 # Data
-streak_data = [1, 1, 0, 1, 1, 0, 0]
+streak_data = [4, 1, 0, 1, 1, 2, 3, 9, 0, 2]
 consecutive_misses = 0
 
 # Current streak
@@ -14,6 +15,13 @@ longest_streak = 0
 longest_streak_total = 0
 
 for streak_value in streak_data:
+    if streak_value >= THRESHOLD:
+        current_streak += 1
+        current_streak_total += streak_value
+        consecutive_misses = 0
+    else:
+        consecutive_misses += 1
+
     if consecutive_misses > MAX_MISS_COUNT:
         if current_streak > longest_streak:
             longest_streak = current_streak
@@ -21,12 +29,9 @@ for streak_value in streak_data:
         current_streak = 0
         current_streak_total = 0
 
-    if streak_value != 0:
-        current_streak += 1
-        current_streak_total += streak_value
-        consecutive_misses = 0
-    else:
-        consecutive_misses += 1
+if current_streak > longest_streak:
+    longest_streak = current_streak
+    longest_streak_total = current_streak_total
 
 print("Current streak:", current_streak)
 print("Current streak total:", current_streak_total)
