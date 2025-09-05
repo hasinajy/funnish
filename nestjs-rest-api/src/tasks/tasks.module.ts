5a742ad3a62759bf7ead5181d7277f93a5a97ac6
hasinajy <jyhasina@gmail.com>
2025-09-05 22:58:56 +0300

    feat: Add the findAll method to the tasks service

--- nestjs-rest-api/src/tasks/tasks.module.ts
+++ nestjs-rest-api/src/tasks/tasks.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { TasksService } from './tasks.service';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [TasksService]
+})
 export class TasksModule {}
---
+++ nestjs-rest-api/src/tasks/tasks.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Task } from 'generated/prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<Task[]> {
+    return this.prisma.task.findMany();
+  }
+}
