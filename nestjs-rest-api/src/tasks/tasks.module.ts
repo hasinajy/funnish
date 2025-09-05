a25f6cad5877479d440323556e7f715128190d58
hasinajy <jyhasina@gmail.com>
2025-09-05 23:02:07 +0300

    feat: Create the GET endpoint for the tasks

--- nestjs-rest-api/src/tasks/tasks.module.ts
+++ nestjs-rest-api/src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
 
 @Module({
   imports: [PrismaModule],
-  providers: [TasksService]
+  providers: [TasksService],
+  controllers: [TasksController]
 })
 export class TasksModule {}
