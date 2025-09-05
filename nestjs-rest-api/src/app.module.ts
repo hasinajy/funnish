494ffd0f176d5a7e4b815b6656e43dd295c0432b
hasinajy <jyhasina@gmail.com>
2025-09-05 22:55:06 +0300

    feat: Create the prisma module

--- nestjs-rest-api/src/app.module.ts
+++ nestjs-rest-api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, PrismaModule]
 })
 export class AppModule {}
