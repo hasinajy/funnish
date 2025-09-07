7d16ee0fa1b966a34094921adc50c76588a36206
hasinajy <jyhasina@gmail.com>
2025-09-07 21:09:41 +0300

    feat: Create auth module

--- nestjs-rest-api/src/app.module.ts
+++ nestjs-rest-api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, PrismaModule]
+  imports: [TasksModule, PrismaModule, AuthModule]
 })
 export class AppModule {}
