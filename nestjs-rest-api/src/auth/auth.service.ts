2644d6f2325a3b938960ca97edce61e057838fe7
hasinajy <jyhasina@gmail.com>
2025-09-07 22:45:27 +0300

    feat: Add validateUser method to auth service

--- nestjs-rest-api/src/auth/auth.service.ts
+++ nestjs-rest-api/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import bcrypt from 'node_modules/bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByUsername(username);
+    if (user && (await bcrypt.compare(password, user.passwordHash))) {
+      const { passwordHash, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
